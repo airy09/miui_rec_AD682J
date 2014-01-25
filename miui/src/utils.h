ef8d3d1380bb11ebb8330e840172dd9dc03f981d
cruizz <airy09@gmail.com>
2014-01-25 12:08:45 +0700

    able to build

--- miui/src/utils.h
+++ miui/src/utils.h
@@ -30,14 +30,14 @@ typedef int u32;
 //
 // MIUI Main Configurations
 
-#define MIUI_NAME        "MIUI Rec by  LaoYang"
+#define MIUI_NAME        "MIUI Rec by  Cruizz"
 //rom_version
 #define MIUI_VERSION     "v3.1.0"
 //rom date
-#define MIUI_BUILD       "2013-09-19"
-#define MIUI_BUILD_CN    "Thusday"
-#define MIUI_BUILD_L     "sndnvaps"
-#define MIUI_BUILD_A     "<sndnvaps@gmail.com>"
+#define MIUI_BUILD       "2014-01-24"
+#define MIUI_BUILD_CN    "friday"
+#define MIUI_BUILD_L     "Cruizz"
+#define MIUI_BUILD_A     "<airy09@gmail.com>"
 #define MIUI_BUILD_URL   "http://www.gaojiquan.com/"
 #define MIUI_COPY        "(c) 2013 by Gaojiquan GTO LaoYang"
 
